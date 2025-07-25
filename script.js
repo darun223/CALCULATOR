240d1e4d4ad0e972188ccc5f59db6071965140b0
darun223 <darunv6@gmail.com>
2025-07-25 22:47:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+
+  if (/[0-9+\-*/().]/.test(key)) {
+    appendValue(key);
+  } else if (key === "Enter") {
+    calculate();
+  } else if (key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  } else if (key.toLowerCase() === "c") {
+    clearDisplay();
+  }
+});
